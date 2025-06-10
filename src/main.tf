93a02bb0a62eb47d3a872c3578d5563585e15440
Arlind-dev <arlind@sulej.ch>
2025-06-10 15:32:25 +0200

    reference the resource group correctly and change public ip to static

--- src/main.tf
+++ src/main.tf
@@ -1,6 +1,5 @@
 provider "azurerm" {
-  features {}
-
+  features        {}
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
 }
@@ -13,28 +12,30 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "public_ip" {
   name                = "${var.vm_name}-public-ip"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku               = "Standard"
+  allocation_method = "Static"
 }
 
 resource "azurerm_network_interface" "nic" {
   name                = var.nic_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
@@ -45,13 +46,15 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                            = var.vm_name
-  location                        = var.location
-  resource_group_name             = var.resource_group_name
-  size                            = var.vm_size
-  admin_username                  = var.admin_username
-  network_interface_ids           = [azurerm_network_interface.nic.id]
+  name                  = var.vm_name
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  size                  = var.vm_size
+  admin_username        = var.admin_username
   disable_password_authentication = true
+
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
   admin_ssh_key {
     username   = var.admin_username
     public_key = var.ssh_public_key
