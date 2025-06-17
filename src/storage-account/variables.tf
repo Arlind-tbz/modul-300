73bead0643f9c1a3049ae297a09f2107bb50ab38
Arlind-dev <arlind@sulej.ch>
2025-06-17 14:12:54 +0200

    add storage account

---
+++ src/storage-account/variables.tf
@@ -0,0 +1,19 @@
+variable "location" {
+  description = "Azure region to deploy resources"
+  default     = "switzerlandnorth"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group for the backend"
+  default     = "tfstate-rg"
+}
+
+variable "storage_account_name" {
+  description = "Globally unique name for the Terraform backend storage account"
+  default     = "tfstatebackend1234"
+}
+
+variable "container_name" {
+  description = "Name of the blob container to store the tfstate file"
+  default     = "tfstate"
+}
