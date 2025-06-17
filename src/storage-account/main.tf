1138dde902e6684ab5c802e0bb420d17f77be9fc
Arlind-dev <arlind@sulej.ch>
2025-06-17 14:17:16 +0200

    test

--- src/storage-account/main.tf
+++ src/storage-account/main.tf
@@ -13,7 +13,6 @@ resource "azurerm_storage_account" "tfstate" {
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  allow_blob_public_access = false
 }
 
 resource "azurerm_storage_container" "tfstate" {
