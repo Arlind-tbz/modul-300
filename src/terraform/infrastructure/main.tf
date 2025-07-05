80c2fa9509e9b0d205d80ac4f3e8801086ecf5a7
Arlind-dev <arlind@sulej.ch>
2025-07-05 20:36:57 +0200

    fix indentation

--- src/terraform/infrastructure/main.tf
+++ src/terraform/infrastructure/main.tf
@@ -120,14 +120,14 @@ resource "azurerm_container_app" "db" {
         path = "/var/lib/mysql"
       }
     }
-  }
 
-  volume {
-    name = "dbvolume"
+    volume {
+      name = "dbvolume"
 
-    azure_file {
-      storage_account_name = azurerm_storage_account.db_storage.name
-      share_name           = azurerm_storage_share.db_share.name
+      azure_file {
+        storage_account_name = azurerm_storage_account.db_storage.name
+        share_name           = azurerm_storage_share.db_share.name
+      }
     }
   }
 }
